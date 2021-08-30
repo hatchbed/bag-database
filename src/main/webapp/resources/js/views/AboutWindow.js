f02ac194abfd7b9c5cce292a4b24d85a1a990291
P. J. Reed <phillipreed@hatchbed.com>
2021-08-30 10:21:30 -0500

    Merge upstream into master

--- src/main/webapp/resources/js/views/AboutWindow.js
+++ src/main/webapp/resources/js/views/AboutWindow.js
@@ -36,8 +36,8 @@ Ext.define('BagDatabase.views.AboutWindow', {
     bodyPadding: 5,
     constrainHeader: true,
     html: "<h2>Bag Database 3.4.0</h2>" +
-        "<p>Documentation: <a href='https://swri-robotics.github.io/bag-database/'>https://swri-robotics.github.io/bag-database/</a></p>" +
-        "<p>Source Code: <a href='https://github.com/swri-robotics/bag-database'>https://github.com/swri-robotics/bag-database</a></p>" +
+        "<p>Documentation: <a href='https://hatchbed.github.io/bag-database/'>https://hatchbed.github.io/bag-database/</a></p>" +
+        "<p>Source Code: <a href='https://github.com/hatchbed/bag-database'>https://github.com/hatchbed/bag-database</a></p>" +
         "<p>Copyright 2015-2020 Southwest Research Institute</p>" +
         "<p>Copyright 2021 Hatchbed, L.L.C.</p>" +
         "<br>" +
